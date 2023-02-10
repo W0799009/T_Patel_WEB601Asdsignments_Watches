dbbe7fc4e05d4a868ebe082549ba6f466762f846
W0799009 <TP94@myscc.ca>
2023-02-10 11:59:03 -0500

    Commit all

--- T_Patel_Watches/src/app/app.module.ts
+++ T_Patel_Watches/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContentCardComponent } from './content-card/content-card.component';
+//import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent
+    //ContentCardComponent,
+    ContentListComponent
   ],
   imports: [
     BrowserModule
--- T_Patel_Watches/src/app/content-card/content-card.component_OLD.ts
+++ T_Patel_Watches/src/app/content-card/content-card.component_OLD.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
+//import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
-export class ContentCardComponent {
+  export
+/*export class ContentCardComponent {
   myContentList: ContentList = new ContentList();
 
   contentItem1: Content = {
@@ -48,7 +49,7 @@ export class ContentCardComponent {
     if (myItems.length > 0 && index > 0 && index < myItems.length) {
       return this.myContentList.printIndex(index);
     } else {
-      alert("NO THAT IS BAD");
+      alert("NO THAT IS BAD");z
     }
   }
-}
+}*/ 
